83628c6acfe486c679cc3ba42146111550cbb57d
Thomas Wotoro <didymusthomas6@gmail.com>
2025-10-07 13:28:51 +0300

    Updated html content

--- lession1/lession1.py
+++ lession1/lession1.py
@@ -9,4 +9,21 @@ main_div = soup.find('div', id='main')
 
 print('Main div content')
 
-print(main_div.prettify())
+print(main_div.prettify())from bs4 import BeautifulSoup
+
+html_content = """
+<html>
+<body>
+    <div id="main">
+        <h1>Welcome </h1>
+        <p>Learn we scraping. </p>
+        <p>It's a useful technique.</p>
+    </div>
+</body>
+</html>
+
+"""
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+main_div = soup.find('div', id='main')
