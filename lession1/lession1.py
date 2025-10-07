5b6ba4eb325daaea5f9e29b22695f5cabcd71fe6
Thomas Wotoro <didymusthomas6@gmail.com>
2025-10-07 15:47:53 +0300

    Updated

--- lession1/lession1.py
+++ lession1/lession1.py
@@ -1,16 +1,5 @@
 from bs4 import BeautifulSoup
 
-html_content = '<html><body><div id="main"><h1>Welcome</h1><p>Learn web scraping.</p></div></body></html>'
-
-
-soup = BeautifulSoup(html_content, 'html.parser')
-
-main_div = soup.find('div', id='main')
-
-print('Main div content')
-
-print(main_div.prettify())from bs4 import BeautifulSoup
-
 html_content = """
 <html>
 <body>
@@ -27,3 +16,7 @@ html_content = """
 soup = BeautifulSoup(html_content, 'html.parser')
 
 main_div = soup.find('div', id='main')
+
+# Printing the div
+print("main div content")
+print(main_div.prettify())
