39e2ea277df6213590c57cf262bd9fbdb274382f
Thomas Wotoro <didymusthomas6@gmail.com>
2025-10-26 22:04:17 +0300

    Add .gitignore to hold some private files modify the script for extract al scrap all the images.

--- Mastering_beautifulSoup/Extracting_and_saving_images/lesson1.py
+++ Mastering_beautifulSoup/Extracting_and_saving_images/lesson1.py
@@ -10,15 +10,17 @@ images = soup.find_all('img')
 
 os.makedirs('images', exist_ok=True)
 
-src = images[0]['src']
+for image in images:
+    src = image['src'] # image 1
+    # src = images[1]['src'] # image 2
 
-full_src = f"https://books.toscrape.com/{src}" if not src.startswith('http') else src
+    full_src = f"https://books.toscrape.com/{src}" if not src.startswith('http') else src
 
-img_response = requests.get(full_src, stream=True)
+    img_response = requests.get(full_src, stream=True)
 
-if img_response.status_code == 200:
-    img_name = os.path.basename(src)
-    with open(f"images/{img_name}", 'wb') as f:
-        for chunk in img_response.iter_content(1024):
-            f.write(chunk)
-    print(f"Saved {img_name}")
+    if img_response.status_code == 200:
+        img_name = os.path.basename(src)
+        with open(f"images/{img_name}", 'wb') as f:
+            for chunk in img_response.iter_content(1024):
+                f.write(chunk)
+        print(f"Saved {img_name}")
